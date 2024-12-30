d354ae041fbeac6078b2edaa3f4b04b4bfb96368
Suryansh Srivastava <suryanshhbtu@gmail.com>
2024-12-30 23:18:06 +0530

    for each in stream api

---
+++ src/main/java/com/java8/streamapi/ForEach.java
@@ -0,0 +1,21 @@
+package com.java8.streamapi;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ForEach {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+        // External Iterators
+        for(int i : list){
+            System.out.println(i);
+        }
+        Iterator<Integer> i = list.iterator();
+        while(i.hasNext()){
+            System.out.println(i.next());
+        }
+        // Internal Iterators
+        list.forEach(j -> System.out.println(j));
+    }
+}
