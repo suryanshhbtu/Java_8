592237c2417c44bb1162221f36a6e42673ed298a
Suryansh Srivastava <suryanshhbtu@gmail.com>
2024-12-31 14:59:49 +0530

    Calling method by its reference

--- src/main/java/com/java8/streamapi/ForEachWorking.java
+++ src/main/java/com/java8/streamapi/ForEachWorking.java
@@ -7,6 +7,7 @@ import java.util.function.Consumer;
 public class ForEachWorking {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+
         // Consumer is an interface -> using anonymous class
         Consumer<Integer> cc = new Consumer<Integer>() {
             @Override
@@ -14,6 +15,7 @@ public class ForEachWorking {
                 System.out.println(i);
             }
         };
+
         // here list is provider and c is consumer
         // -> each time c, calls accept method on every child
         list.forEach(cc);
@@ -24,5 +26,18 @@ public class ForEachWorking {
 
         // this can be also done
         list.forEach(i->System.out.println(i));
+
+        // we have call by value, call by reference, call by method
+        // for each method's reference -> call by method
+        list.forEach(System.out::println);
+
+        // one more example, for call by method reference
+//        list.forEach(i->doubleIt(i));
+        list.forEach(ForEachWorking::doubleIt);
     }
+
+    public static void doubleIt(int i){
+        System.out.println(i*2);
+    }
+
 }
